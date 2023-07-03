33ac209c9b248e44adcc84be689893c3f612f43e
Edward Karavakis <edward.karavakis@cern.ch>
2023-07-03 11:44:18 +0200

    Latest changes, added config file

--- pandacommon/kafkapublisher/KafkaPublisher.py
+++ pandacommon/kafkapublisher/KafkaPublisher.py
@@ -8,7 +8,7 @@ import json
 import hashlib
 from confluent_kafka import Producer
 from pandacommon.pandalogger.logger_utils import logger_utils
-import config
+import kafka_config as config
 
 class KafkaPublisher:
     def __init__(self):
